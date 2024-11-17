40cf2988069d661c8938b810680a9875a608a7e8
satyam rai <raisatyam121@gmail.com>
2024-11-17 23:40:22 +0530

    added graphql server

---
+++ graphql/dist/index.js
@@ -0,0 +1,70 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import bodyParser from "body-parser";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+import { dirname } from "path";
+import axios from "axios";
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
+const __dirname = dirname(__filename);
+console.log(__dirname);
+const typeDefs = fs.readFileSync(path.join(__dirname, "./schema.graphql"), "utf-8");
+const resolvers = {
+    Query: {
+        userDetails: async (_, args) => {
+            try {
+                const res = await axios.post("http://localhost:5002/user-api/user-details", { id: args.id });
+                return res.data.userDetails;
+            }
+            catch (error) {
+                console.error("Error fetching user details:", error);
+                throw new Error("Failed to fetch user details");
+            }
+        },
+        allOrderDetails: async (_, args) => {
+            try {
+                const res = await axios.post("http://localhost:5000/order-api/all-order-details", {
+                    userId: args.userId,
+                });
+                return res.data.orderDetails;
+            }
+            catch (error) {
+                console.error("Error fetching all Order details:", error);
+                throw new Error("Failed to fetch Order details");
+            }
+        },
+        orderDetails: async (_, args) => {
+            try {
+                const res = await axios.post("http://localhost:5000/order-api/order-details", {
+                    orderId: args.orderId,
+                });
+                console.log(res.data.orderDetails);
+                // console.log(res.data.orderDetails.orderItems);
+                return res.data.orderDetails;
+            }
+            catch (error) {
+                console.error("Error fetching all Order details:", error);
+                throw new Error("Failed to fetch Order details");
+            }
+        },
+    },
+};
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+await server.start();
+app.use("/graphql", bodyParser.json(), expressMiddleware(server));
+app.get("/check", (req, res) => {
+    res.json({ message: "Hi from Express server" });
+});
+// Start the Express server
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running at http://localhost:${PORT}/graphql`);
+    console.log(`Express route is available at http://localhost:${PORT}/check`);
+});
